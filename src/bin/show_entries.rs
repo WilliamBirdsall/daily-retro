4b273ae724820368bfb7cb32cbc229bcb7c76c41
Will Birdsall <will@ignition72.com>
2024-01-13 00:43:58 -0500

    Add comments for future implementation plans

--- src/bin/show_entries.rs
+++ src/bin/show_entries.rs
@@ -6,6 +6,26 @@ fn main() {
     use self::schema::entries::dsl::*;
 
     let connection = &mut establish_connection();
+
+    // Print welcome message
+    //     Print key commands, c for create, e for edit, and d for delete, l for list
+
+
+    let mut went_well = String::new();
+    let mut went_poorly = String::new();
+    let mut learned = String::new();
+
+    println!("What went well today?");
+    io::stdin().read_line(&mut went_well).unwrap();
+
+    println!("What went poorly today?");
+
+    println!("What was learned today?");
+
+
+    // Create entry with prompt answers
+    // Save entry
+
     let results = entries
         .limit(5)
         .select(Entry::as_select())
